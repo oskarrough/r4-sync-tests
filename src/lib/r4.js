1e27153f44fa4f3fbcbc4ddb4c0768e8c6d61315
oskarrough <oskar@rough.dk>
2025-08-06 18:36:46 +0200

    Fix syntax

--- src/lib/r4.js
+++ src/lib/r4.js
@@ -5,45 +5,23 @@ import {sdk} from '@radio4000/sdk'
  * Provides a consistent interface to the remote database
  */
 
+async function unwrap(fn) {
+	const {data, error} = await fn()
+	if (error) throw error
+	return data
+}
+
 export const r4 = {
 	users: {
-		async readUser() {
-			const {data, error} = await sdk.users.readUser()
-			if (error) throw error
-			return data
-		}
+		readUser: (...args) => unwrap(() => sdk.users.readUser(...args))
 	},
 
 	channels: {
-		async readChannel(slug) {
-			const {data, error} = await sdk.channels.readChannel(slug)
-			if (error) throw error
-			return data
-		},
-
-		async readUserChannels() {
-			const {data, error} = await sdk.channels.readUserChannels()
-			if (error) throw error
-			return data
-		},
-
-		async readChannelTracks(slug) {
-			const {data, error} = await sdk.channels.readChannelTracks(slug)
-			if (error) throw error
-			return data
-		},
-
-		async readFollowings(channelId) {
-			const {data, error} = await sdk.channels.readFollowings(channelId)
-			if (error) throw error
-			return data
-		},
-
-		async followChannel(followerChannelId, channelId) {
-			const {data, error} = await sdk.channels.followChannel(followerChannelId, channelId)
-			if (error) throw error
-			return data
-		}
+		readChannel: (...args) => unwrap(() => sdk.channels.readChannel(...args)),
+		readUserChannels: (...args) => unwrap(() => sdk.channels.readUserChannels(...args)),
+		readChannelTracks: (...args) => unwrap(() => sdk.channels.readChannelTracks(...args)),
+		readFollowings: (...args) => unwrap(() => sdk.channels.readFollowings(...args)),
+		followChannel: (...args) => unwrap(() => sdk.channels.followChannel(...args))
 	},
 
 	// Escape hatch - original SDK when you need {data, error} pattern
