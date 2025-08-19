1150d3f3f4ca70977c8c0a4278c3aa7724b46b55
oskarrough <oskar@rough.dk>
2025-08-19 19:36:22 +0200

    Fix theme mount settings

--- src/lib/components/theme-toggle.svelte
+++ src/lib/components/theme-toggle.svelte
@@ -3,19 +3,9 @@
 	import {appState} from '$lib/app-state.svelte'
 	import Icon from '$lib/components/icon.svelte'
 
-	const {matches: prefersLight} = window.matchMedia('(prefers-color-scheme: light)')
+	const prefersLight = $derived(window.matchMedia('(prefers-color-scheme: light)').matches)
 	const theme = $derived(appState.theme ?? (prefersLight ? 'light' : 'dark'))
 	const icon = $derived(theme === 'light' ? 'moon' : 'sun')
-
-	$effect(() => {
-		if (theme === 'dark') {
-			document.documentElement.classList.remove('light')
-			document.documentElement.classList.add('dark')
-		} else {
-			document.documentElement.classList.remove('dark')
-			document.documentElement.classList.add('light')
-		}
-	})
 </script>
 
 <button onclick={toggleThemeApi}>
