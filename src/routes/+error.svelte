425081e4d66d374c25eeccae662b474a2a2577c4
Oskar Rough <oskar@rough.dk>
2025-08-19 10:12:10 +0200

    Link /recovery on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,14 +3,20 @@
 </script>
 
 <article>
-	<p>An error happened.</p>
+	<p>An error happened:</p>
 	<h1>{page.status}: {page.error?.message || 'Unknown error'}</h1>
-	<p><a href="/settings">Go to settings to reset the application.</a></p>
+	<p><a href="/recovery">Reset the app</a> (this wipes your local data only, e.g. safe, and usually solves it)</p>
 	<p><button onclick={() => window.location.reload()}>Reload app</button></p>
 </article>
 
 <style>
 	article {
-		margin: 0 0.5rem;
+		margin: 0.5rem;
+		display: flex;
+		flex-flow: column;
+	}
+
+	h1 + p {
+		margin: 1rem;
 	}
 </style>
