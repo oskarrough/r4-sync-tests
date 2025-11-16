b7aee94d5807144f8daaa1454d7ebc9ce7494926
Oskar Rough <oskar@rough.dk>
2025-11-16 22:49:37 +0100

    Fix dynamic query key

--- src/routes/tanstack/collections.ts
+++ src/routes/tanstack/collections.ts
@@ -8,20 +8,28 @@ const queryClient = new QueryClient()
 // Create a collection for tracks with on-demand loading. The idea is to have all tracks for all channels in a single collection and you filter them by slug.
 export const tracksCollection = createCollection(
 	queryCollectionOptions({
-		queryKey: ['tracks'],
-		// queryKey: (ctx) => {
-		// 	// Make queryKey dynamic based on the filters
-		// 	const {filters} = parseLoadSubsetOptions(ctx.meta?.loadSubsetOptions)
-		// 	const slugFilter = filters.find((f) => f.field.includes('slug') && f.operator === 'eq')
-		// 	const queryKey = ['tracks', slugFilter?.value || 'all']
-		// 	return queryKey
-		// },
+		// queryKey: ['tracks'],
+		// Dynamic query key based on filters
+		queryKey: (opts) => {
+			const parsed = parseLoadSubsetOptions(opts)
+			const cacheKey = ['products']
+
+			parsed.filters.forEach((f) => {
+				cacheKey.push(`${f.field.join('.')}-${f.operator}-${f.value}`)
+			})
+
+			if (parsed.limit) {
+				cacheKey.push(`limit-${parsed.limit}`)
+			}
+
+			return cacheKey
+		},
 		syncMode: 'on-demand',
 		// staleTime: 5 * 60 * 1000, // 5 minutes
 		queryFn: async (ctx) => {
-			const params = parseLoadSubsetOptions(ctx.meta?.loadSubsetOptions)
-			const slug = params.filters.find((f) => f.field.includes('slug') && f.operator === 'eq')?.value
-			const {data, error} = await sdk.channels.readChannelTracks(slug, params.limit)
+			const {filters, limit} = parseLoadSubsetOptions(ctx.meta?.loadSubsetOptions)
+			const slug = filters.find((f) => f.field.includes('slug') && f.operator === 'eq')?.value
+			const {data, error} = await sdk.channels.readChannelTracks(slug, limit)
 			if (error) throw error
 			return data
 		},
