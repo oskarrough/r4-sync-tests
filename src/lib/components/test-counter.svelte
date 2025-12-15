ac0ac997ac9a9c9ebf0b03e5ea7234e19a280d7b
Oskar Rough <oskar@rough.dk>
2025-12-15 11:41:19 +0100

    Add internet status indicator to header

--- src/lib/components/test-counter.svelte
+++ src/lib/components/test-counter.svelte
@@ -1,13 +1,37 @@
 <script>
 	import {appState} from '$lib/app-state.svelte'
+	import Icon from '$lib/components/icon.svelte'
 	import * as m from '$lib/paraglide/messages'
+
+	let isOnline = $state(navigator.onLine)
 
 	function inc() {
 		appState.counter++
 	}
+
+	$effect(() => {
+		function onOnline() {
+			isOnline = true
+		}
+		function onOffline() {
+			isOnline = false
+		}
+		window.addEventListener('online', onOnline)
+		window.addEventListener('offline', onOffline)
+		return () => {
+			window.removeEventListener('online', onOnline)
+			window.removeEventListener('offline', onOffline)
+		}
+	})
 </script>
 
-<button onclick={inc}> {m.test_counter_prefix()}{appState.counter}</button>
+<button onclick={inc} class:offline={!isOnline}>
+	{#if !isOnline}
+		OFF<br />LINE
+	{:else}
+		{m.test_counter_prefix()}{appState.counter}
+	{/if}
+</button>
 
 <style>
 	button {
@@ -16,5 +40,12 @@
 
 	:global(a.active) button {
 		border-color: var(--accent-9);
+	}
+
+	button.offline {
+		font-size: var(--font-1);
+		line-height: 1.2;
+		color: var(--gray-9);
+		background: var(--gray-3);
 	}
 </style>
